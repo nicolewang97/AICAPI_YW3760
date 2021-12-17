3b541b7bafd46ed5bdbf625ddcc68e22bab6d798
nicolewang97 <90565190+nicolewang97@users.noreply.github.com>
2021-12-16 19:56:12 -0500

    update

--- tests/test_aicapi_yw3760.py
+++ tests/test_aicapi_yw3760.py
@@ -12,16 +12,18 @@ from IPython.core.display import display, HTML
 
 def test_art_search():
     example = 'Monet'
-    actual = aicapi_yw3760.art_search(example)
-    assert type(actual) == pd.DataFrame
+    actual2 = aicapi_yw3760.art_search(example)
+    assert type(actual2) == pd.DataFrame
     
 def test_tour_search():
     example = 'Monet'
-    actual = aicapi_yw3760.tour_search(example)
-    assert type(actual) == pd.DataFrame
+    actual3 = aicapi_yw3760.tour_search(example)
+    assert type(actual3) == pd.DataFrame
+    
+def test_pic_search():
+    actual = aicapi_yw3760.pic_search('Water Lillies','Monet')
+    assert actual is not None
     
 def test_product_search():
-    example = ('Rainy Day','Mug')
-    actual = aicapi_yw3760.product_search(example)
-    expect = type(HTML())
-    assert type(actual) == expect
+    actual4 = aicapi_yw3760.product_search('Rainy Day','Monet')
+    assert actual4 is not None
